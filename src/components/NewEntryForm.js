e2d10ff4df3f3456866c438d793066abb3d6a5dd
Moncada <pedro.moncada@chevron.com>
2024-01-31 22:06:39 -0800

    Refactor components

---
+++ src/components/NewEntryForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Form } from "semantic-ui-react";
+import ButtonSaveOrCancel from './ButtonSaveOrCancel';
+
+function NewEntryForm(){
+    return (
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input icon="tags" width={12} label="Description" placeholder="New shiny thing" />
+          <Form.Input icon="dollar" iconPosition="left" width={4} label="Value" placeholder="100.00" />
+        </Form.Group>
+        <ButtonSaveOrCancel />
+      </Form>
+    );
+}
+
+export default NewEntryForm;
